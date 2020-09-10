9cf7c9d7fe97f91fd1cf41555b19af510d4121c0
Anton Panasenko <apanasenko@twitter.com>
2020-09-09 18:39:13 -0700

    use new swift package manager

--- Package.swift
+++ Package.swift
@@ -1,7 +1,12 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+
 import PackageDescription
 
 let package = Package(
   name: "SpeechRecognizerButton",
+  products: [ .library(name: "SpeechRecognizerButton", targets: ["SpeechRecognizerButton"]) ],
   dependencies : [],
-  exclude: ["Example", "ExampleUITests", "SpeechRecognizerButtonTests"]
+  targets: [.target(name: "SpeechRecognizerButton", dependencies: [])] 
 )
